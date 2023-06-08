ce5e1f7c9545c55acc4eff7a2085a32fd20b26e8
Jaylen Stokes <jaylenstokes4@gmail.com>
2023-06-08 19:43:21 -0400

    fixed inventory system and added functions 3-5

---
+++ inventory-system.js
@@ -0,0 +1,51 @@
+let inventoryItems = []
+
+function additem(name, price, quantity) {
+      let item = {
+        name: name,
+        price: price,
+        quantity: quantity,
+      };
+      inventoryItems.push(item); 
+}
+
+additem("Orange", .75, 50);
+additem("Lemon", .75, 50);
+additem("Cereal", 5, 8);
+additem("Orange", .75, 50);
+additem("Orange", .75, 50);
+additem("Juice", 4.50, 30);
+
+console.log(inventoryItems);
+
+function removeItem(name){
+   inventoryItems = inventoryItems.filter((item) => item.name !== name);
+}
+
+removeItem("Orange")
+console.log(inventoryItems)
+
+function updateItem(name, newPrice, newQuantity){
+  for(let i = 0; i < inventoryItems.length; i++){
+    if(inventoryItems[i].name === name){
+      inventoryItems[i].price = newPrice;
+      inventoryItems[i].quantity = newQuantity;
+        }
+    }
+}
+
+updateItem("Lemon", .85, 200);
+console.log(inventoryItems)
+
+function calculateTotalValue(){
+  let totalValue =0;
+  for(let i = 0; i < inventoryItems.length; i++){
+  const item = inventoryItems[i];
+  const itemValue = item.price * item.quantity;
+  totalValue += itemValue;
+  }
+  return totalValue;
+}
+
+const totalValue = calculateTotalValue();
+console.log("Total Value:", totalValue)
